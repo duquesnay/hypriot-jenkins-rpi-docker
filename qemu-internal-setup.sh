603cd63757c64e729f6cad78a8a9a7f5134b44f1
Guillaume Duquesnay <guillaume.duquesnay@gmail.com>
2017-03-13 02:21:22 +0800

    can launch docker-in-emulated-hypriot-in-docker

--- qemu-internal-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sed -i -e 's/^/#/' /etc/ld.so.preload
-sed -i -e 's/^snd_bcm/#snd_bcmx/' /etc/modules-load.d/modules.conf
-# TODO: clean this up with a bash var for path
-touch /etc/udev/rules.d/90-qemu.rules
-echo 'KERNEL=="sda", SYMLINK+="mmcblk0"' >> /etc/udev/rules.d/90-qemu.rules
-echo 'KERNEL=="sda?", SYMLINK+="mmcblk0p%n"' >> /etc/udev/rules.d/90-qemu.rules
-echo 'KERNEL=="sda2", SYMLINK+="root"' >> /etc/udev/rules.d/90-qemu.rules
-
